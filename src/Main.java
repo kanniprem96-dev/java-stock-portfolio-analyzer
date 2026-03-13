92c19b8b25c17bdc4647b94d80049c41b97b0b1c
Kanniban Premananthan <kanniprem96@gmail.com>
2026-03-13 16:52:21 +0000

    Add Stock and Portfolio classes with sample data

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Portfolio myPortfolio = new Portfolio();
+
+        // Create some stocks
+        Stock apple = new Stock("AAPL", 10, 150, 175);
+        Stock tesla = new Stock("TSLA", 5, 200, 190);
+        Stock nvidia = new Stock("NVDA", 3, 500, 550);
+
+        // Add stocks to portfolio
+        myPortfolio.addStock(apple);
+        myPortfolio.addStock(tesla);
+        myPortfolio.addStock(nvidia);
+
+        // Print the portfolio
+        myPortfolio.printPortfolio();
+    }
 }
